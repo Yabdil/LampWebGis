36d6e80b5c5b1f78bcfe0e553a7815d56cc79509
Yabdil <abdillahi.abdimoh@gmail.com>
2020-11-13 21:59:42 +0100

    dd

--- lampProject/settings.py
+++ lampProject/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+from os import environ
 # Create .env file path.
 #dotenv_path = join(dirname(__file__), '../.env')
 # Load file from the path.
