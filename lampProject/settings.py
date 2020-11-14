3586283ba7b17f99f819a83a66a150be8961ec8f
Yabdil <abdillahi.abdimoh@gmail.com>
2020-11-14 21:51:52 +0100

    set debug to false

--- lampProject/settings.py
+++ lampProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a$n@_hsbe+i3^^mmh1!ewxj^^=1@xk-t5bh8h550^eak1=ekcz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','142.93.171.149']
 
