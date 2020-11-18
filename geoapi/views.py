70e113c050d0e498431b599f21192bd6e05fb800
Yabdil <abdillahi.abdimoh@gmail.com>
2020-11-18 02:45:47 +0100

    deleting requests module

--- geoapi/views.py
+++ geoapi/views.py
@@ -6,7 +6,6 @@ from .serializer import LampSerializer, Lamp_historiqueSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import json
-import requests
 import datetime
 
 class LampView(APIView):
