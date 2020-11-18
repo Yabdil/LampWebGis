89454d5cb288fe3fcc558b3876b26d93319a1a37
Yabdil <abdillahi.abdimoh@gmail.com>
2020-11-18 04:28:42 +0100

    pushing to prod the app

--- lampProject/settings.py
+++ lampProject/settings.py
@@ -30,7 +30,7 @@ with open('lampProject/env.json') as file:
     hosts = jsonFile['ALLOWED_HOSTS']
 
 
-ENVIRONNEMENT = 'PREPROD'
+ENVIRONNEMENT = 'DEV'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONNEMENT == 'DEV':
