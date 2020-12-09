73f6ba2344705665ec50a1b666c129e27292d582
Yabdil <abdillahi.abdimoh@gmail.com>
2020-12-09 16:47:29 +0100

    settings changes

--- lampProject/settings.py
+++ lampProject/settings.py
@@ -27,7 +27,7 @@ ENVIRONNEMENT = 'DEV'
 if ENVIRONNEMENT == 'DEV':
     DEBUG = True
 else:
-    DEBUG = True #we will turn debug to false in a prod or other environments
+    DEBUG = False #we will turn debug to false in a prod or other environments
 
 
 ALLOWED_HOSTS = ['*']
